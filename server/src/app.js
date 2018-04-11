a954f70ad560a4adbb5e6632f07af55e3ffd3484
deepakyec <deep3265@gmail.com>
2018-04-11 13:15:22 +0530

    create project and client and server

---
+++ server/src/app.js
@@ -0,0 +1,24 @@
+console.log('Helloooo');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+app.use(morgan('combine'));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('', (req, res) => {
+    res.send({
+        time: new Date(),
+        message: 'api running...'
+    });
+});
+app.get('/status', (req, res) => {
+    res.send({
+        message: 'Hello World!'
+    });
+});
+
+app.listen(process.env.PORT || 8081);
